e038db97db8100efbb4eed961886d96f00888f09
Tejesh Chauragade <chauragade.tejesh@gmail.com>
2021-05-13 12:28:25 +0530

    added all verbs

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const os = require('os');
 
-
 const app = express();
+app.set('view engine', 'ejs');
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
@@ -20,24 +20,62 @@ mongoose
 const Item = require('./models/item');
 
 
+// Welcome Page
 app.get('/', (req, res) => {
-  res.send("Hello from "+ os.hostname() +"...!");
+  var message = `Welcome to demo express-app hosted on ${os.hostname()}.`;
+  res.render('pages/index', {message:message});
 })
 
+
+// Items Routes
+// List Items
 app.get('/items', (req, res) => {
   Item.find()
     .then(items => res.status(200).json(items))
     .catch(err => res.status(404).json({ msg: 'No items found' }));
 });
 
+// Get item by Id
+app.get('/items/:id', (req, res) => {
+  Item.findById(req.params.id)
+    .then(item => res.status(200).json(item))
+    .catch(err => res.status(404).json({ msg: 'Not found' }));
+});
+
+// Create new item
 app.post('/items', (req, res) => {
   console.log(req.body);
   const newItem = new Item({
     name: req.body.name
   });
-
   newItem.save().then(item => res.status(200).json(item));
 });
+
+
+// Update item by Id
+app.put('/items/:id', (req, res) => {
+  Item.findById(req.params.id)
+    .then(item => { 
+      item.name = req.body.name;
+      item.save();
+      res.status(200).json(item)
+    })
+    .catch(err => res.status(404).json({ msg: 'Not Found' }));
+});
+
+
+// Delete item by Id
+app.delete('/items/:id', (req, res) => {
+  Item.findById(req.params.id)
+    .then(item => { 
+      item.remove();
+      res.status(204).json({'msg': 'removed'});
+    })
+    .catch(err => res.status(404).json({ msg: 'Not Found' }));
+});
+
+
+
 
 const port = 3000;
 
